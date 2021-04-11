838cf61ce6003e317d8150457f073f571d8fc6af
Alhvi Balcarcel <abalcarcel@gmail.com>
2021-04-10 22:12:42 -0600

    hunting fix

--- Assets/Scripts/Capture.cs
+++ Assets/Scripts/Capture.cs
@@ -1,36 +1,48 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
-
-public class Capture : MonoBehaviour
-{
+public class Capture : MonoBehaviour {
     public Text scoreText;
+    public Text debugText;
     private int score = 0;
+    public Camera cam;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (scoreText)
+    private void Update() {
+        if (scoreText) {
             scoreText.text = "Capturated people: " + score.ToString() + "/10";
+        }
 
-        if (Input.GetMouseButtonDown(0))
-        {
-            Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hitInfo;
+        if (Input.GetMouseButtonDown(0)) {
+            //Ray myRay = cam.ScreenPointToRay(Input.mousePosition);
+            //Ray myRay = new Ray(transform.position, transform.forward);
+            Ray myRay = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
 
-            if (Physics.Raycast(myRay, out hitInfo))
-            {
-                if (hitInfo.collider.CompareTag("Destroy"))
-                {
+            if (Physics.Raycast(myRay, out RaycastHit hitInfo)) {
+                //DrawLine(myRay.origin, hitInfo.point, Color.red, 0.5f);
+                //Debug.Log(hitInfo.collider.tag + " " + hitInfo.collider.gameObject.name);
+                //debugText.text = hitInfo.collider.tag + " " + hitInfo.collider.gameObject.name;
+                if (hitInfo.collider.CompareTag("Destroy")) {
                     Destroy(hitInfo.collider.gameObject);
                     score++;
                 }
             }
 
-
         }
 
+    }
+
+    private void DrawLine(Vector3 start, Vector3 end, Color color, float duration = 0.2f) {
+        GameObject myLine = new GameObject();
+        myLine.transform.position = start;
+        myLine.AddComponent<LineRenderer>();
+        LineRenderer lr = myLine.GetComponent<LineRenderer>();
+        lr.material = new Material(Shader.Find("Standard"));
+        lr.startColor = color;
+        lr.endColor = color;
+        lr.startWidth = 0.1f;
+        lr.endWidth = 0.1f;
+        lr.SetPosition(0, start);
+        lr.SetPosition(1, end);
+        GameObject.Destroy(myLine, duration);
     }
 }
