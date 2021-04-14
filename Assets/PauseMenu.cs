005f0247eabcdeed2dde029d6b1ff62175e3c524
alejandragudiel <gud19232@uvg.edu.gt>
2021-04-14 11:56:06 -0600

    menus pausa actualizados

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -2,37 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class Pause : MonoBehaviour
+public class PauseMenu : MonoBehaviour
 {
     public static bool GameIsPaused = false;
     public GameObject pauseMenuUI;
-    
-    void Update (){
-        if (Input.GetKeyDown(KeyCode.Escape)){
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
             if(GameIsPaused){
                 Resume();
             }
             else{
-                Pausee();
+                Pause();
             }
         }
-    }
-    void Pausee(){
-        pauseMenuUI.SetActive(true);
-        Time.timeScale = 0f;
-        GameIsPaused = true;
+        
     }
     public void Resume(){
-        pauseMenuUI.SetActive(false);
-        Time.timeScale = 1f;
-        GameIsPaused = false;
+            pauseMenuUI.SetActive(false);
+            Time.timeScale = 1f;
+            GameIsPaused = false;
     }
-    public void loadMenu(){
+
+        void Pause(){
+            pauseMenuUI.SetActive(true);
+            Time.timeScale = 0f;
+            GameIsPaused = true;
+    }
+    
+    public void LoadMenu(){
         Time.timeScale = 1f;
         SceneManager.LoadScene("MainMenu");
     }
-
     public void QuitGame(){
         Application.Quit();
     }
+
 }
+
