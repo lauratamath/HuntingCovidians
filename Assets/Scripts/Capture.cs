dc5efdf59752176d2ad8d60fd34d58e79bab1f92
lauratamath <laurylevi@hotmail.com>
2021-04-15 21:26:36 -0600

    listoooooooooooooo

--- Assets/Scripts/Capture.cs
+++ Assets/Scripts/Capture.cs
@@ -12,7 +12,7 @@ public class Capture : MonoBehaviour {
     public Text tiempoTotal;
     
     private float tiempo = 3.0f;
-    private float tiempoTota = 3.0f;
+    private float tiempoTota = 150.0f;
     
 
     // Update is called once per frame
